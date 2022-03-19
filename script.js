337b7b0ef7fb4188ac681e318360781dc8a2676c
gulomiddin <sergokomola@gmail.com>
2022-03-19 15:26:29 +0500

    load

---
+++ script.js
@@ -0,0 +1,15 @@
+function imgSlider(globol) {
+    document.querySelector('.cola').src = globol;
+}
+
+
+function changeBgColor(color) {
+    let sec = document.querySelector('.content');
+    sec.style.background = color;
+
+}
+
+function changeColor(color) {
+    let sec = document.querySelector('.span_content');
+    sec.style.color = color;
+}
